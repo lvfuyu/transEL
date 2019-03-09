e405f951b99618cffbdf305d9e909db1fac68029
TTbunny <TTbunny@163.com>
2019-03-09 14:21:37 +0800

    update

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ for v in 1 2 3
 do
 python3 -m model.train  \
 			                     --batch_size=4   --experiment_name=corefmerge \
-								 --training_name=group_local/local_trans_400_1_2_model_v$v \
+								 --training_name=group_local/local_trans_400_1_4_model_v$v \
 								 --ent_vecs_regularization=l2dropout  --evaluation_minutes=1 --nepoch_no_imprv=6 \
 								 --span_emb="boundaries"  \
 								 --dim_char=50 --hidden_size_char=50 --hidden_size_lstm=150 \
@@ -10,6 +10,7 @@ python3 -m model.train  \
 								 --fast_evaluation=True  \
 								 --attention_ent_vecs_no_regularization=True --final_score_ffnn=0_0 \
 								 --attention_R=10 --attention_K=100 \
+								 --dropout=0.5 \
 								 --train_datasets=aida_train \
 								 --ed_datasets=aida_dev_z_aida_test_z_aida_train --ed_val_datasets=0 \
 								 --global_thr=0.001 --global_score_ffnn=0_0
