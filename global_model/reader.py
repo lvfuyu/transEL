13799575df4359bc5dcfd85866b22babc7b077b7
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-17 16:00:43 +0800

    update

--- global_model/reader.py
+++ global_model/reader.py
@@ -42,36 +42,45 @@ def parse_sequence_example(serialized):
            context["mask_index"], sequence["entities"]
 
 
-def count_records_of_one_epoch(trainfiles):
-    filename_queue = tf.train.string_input_producer(trainfiles, num_epochs=1)
-    reader = tf.TFRecordReader()
-    key, serialized_example = reader.read(filename_queue)
-    with tf.Session() as sess:
-        sess.run(
-            tf.variables_initializer(
-                tf.global_variables() + tf.local_variables()
-            )
-        )
+def parse_sequence_example_test(serialized):
+    sequence_features = {
+            # in order to have a vector. if i put [1] it will probably be a matrix with just one column
+            "words": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "chars": tf.VarLenFeature(tf.int64),
+            "chars_len": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "entities": tf.FixedLenSequenceFeature([], dtype=tf.string),  # entity ids
+            "begin_span": tf.FixedLenSequenceFeature([], dtype=tf.int64),  # mention positions
+            "end_span": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "cand_entities": tf.VarLenFeature(tf.int64),
+            "cand_entities_scores": tf.VarLenFeature(tf.float32),
+            "cand_entities_labels": tf.VarLenFeature(tf.int64),
+            "cand_entities_len": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "ground_truth": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "begin_gm": tf.FixedLenSequenceFeature([], dtype=tf.int64),
+            "end_gm": tf.FixedLenSequenceFeature([], dtype=tf.int64)
+    }
 
-        # Start queue runners
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    context, sequence = tf.parse_single_sequence_example(
+        serialized,
+        context_features={
+            "chunk_id": tf.FixedLenFeature([], dtype=tf.string),
+            "words_len": tf.FixedLenFeature([], dtype=tf.int64),
+            "spans_len": tf.FixedLenFeature([], dtype=tf.int64),
+            "ground_truth_len": tf.FixedLenFeature([], dtype=tf.int64),
+            "mask_index": tf.FixedLenFeature([], dtype=tf.int64),
+        },
+        sequence_features=sequence_features)
 
-        counter = 0
-        try:
-            while not coord.should_stop():
-                fetch_vals = sess.run((key))
-                # print(fetch_vals)
-                counter += 1
-        except tf.errors.OutOfRangeError:
-            pass
-        except KeyboardInterrupt:
-            print("Training stopped by Ctrl+C.")
-        finally:
-            coord.request_stop()
-        coord.join(threads)
-    print("number of tfrecords in trainfiles = ", counter)
-    return counter
+    return context["chunk_id"], sequence["words"], context["words_len"],\
+           tf.sparse_tensor_to_dense(sequence["chars"]), sequence["chars_len"],\
+           sequence["begin_span"], sequence["end_span"], context["spans_len"],\
+           tf.sparse_tensor_to_dense(sequence["cand_entities"]),\
+           tf.sparse_tensor_to_dense(sequence["cand_entities_scores"]),\
+           tf.sparse_tensor_to_dense(sequence["cand_entities_labels"]),\
+           sequence["cand_entities_len"],\
+           sequence["ground_truth"], context["ground_truth_len"],\
+           sequence["begin_gm"], sequence["end_gm"],\
+           context["mask_index"], sequence["entities"]
 
 
 def train_input_pipeline(filenames, args):
@@ -85,12 +94,6 @@ def train_input_pipeline(filenames, args):
 
 def test_input_pipeline(filenames, args):
     dataset = tf.data.TFRecordDataset(filenames)
-    dataset = dataset.map(parse_sequence_example)
+    dataset = dataset.map(parse_sequence_example_test)
     dataset = dataset.padded_batch(args.batch_size, dataset.output_shapes)
     return dataset
-
-
-if __name__ == "__main__":
-    count_records_of_one_epoch(["/users/guotonglei/entity_linking/end2end_neural_el/data/tfrecords/"
-                               "corefmerge/"
-                               "gmonly/wikipedia"])
--- trash/code_dump.py
+++ trash/code_dump.py
@@ -10,3 +10,37 @@
 # tf_writers["ed_pr"] = tf.summary.FileWriter(args.summaries_folder + 'ed_pr/')
 # tf_writers["ed_re"] = tf.summary.FileWriter(args.summaries_folder + 'ed_re/')
 # tf_writers["ed_f1"] = tf.summary.FileWriter(args.summaries_folder + 'ed_f1/')
+# def count_records_of_one_epoch(trainfiles):
+#     filename_queue = tf.train.string_input_producer(trainfiles, num_epochs=1)
+#     reader = tf.TFRecordReader()
+#     key, serialized_example = reader.read(filename_queue)
+#     with tf.Session() as sess:
+#         sess.run(
+#             tf.variables_initializer(
+#                 tf.global_variables() + tf.local_variables()
+#             )
+#         )
+#
+#         # Start queue runners
+#         coord = tf.train.Coordinator()
+#         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+#
+#         counter = 0
+#         try:
+#             while not coord.should_stop():
+#                 fetch_vals = sess.run((key))
+#                 # print(fetch_vals)
+#                 counter += 1
+#         except tf.errors.OutOfRangeError:
+#             pass
+#         except KeyboardInterrupt:
+#             print("Training stopped by Ctrl+C.")
+#         finally:
+#             coord.request_stop()
+#         coord.join(threads)
+#     print("number of tfrecords in trainfiles = ", counter)
+#     return counter
+# if __name__ == "__main__":
+#     count_records_of_one_epoch(["/users/guotonglei/entity_linking/end2end_neural_el/data/tfrecords/"
+#                                 "corefmerge/"
+#                                 "gmonly/wikipedia"])
