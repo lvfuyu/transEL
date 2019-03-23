5aed74ad361f645a7e36bffe64e00f45f49b5164
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-23 22:33:30 +0800

    fix

--- trash/code_dump.py
+++ trash/code_dump.py
@@ -100,3 +100,6 @@
 #            sequence["begin_gm"], sequence["end_gm"],\
 #            context["mask_index"], sequence["entities"]
 # print(len(entities[0]))
+
+#--no_pre_training \
+# --training_name=group_global/global_trans_1_4_model_v$v \
