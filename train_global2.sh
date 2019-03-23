4b99f44aeeb7caad02b9cb2f0f77537ecbd574f0
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-23 23:02:36 +0800

    fix

---
+++ train_global2.sh
@@ -0,0 +1,13 @@
+for v in 1
+do
+python3 -m global_model.train \
+        --batch_size=4 --experiment_name=corefmerge \
+        --training_name=group_global/global_trans_1_4_model_v$v \
+        --evaluation_minutes=1 --nepoch_no_imprv=6 \
+        --span_emb="boundaries" \
+        --dropout=0.5 \
+        --entity_extension=extension_entities \
+        --no_pre_training \
+        --train_datasets=aida_train \
+        --ed_datasets=aida_dev_z_aida_test --ed_val_datasets=0 --ed_test_datasets=1
+done
