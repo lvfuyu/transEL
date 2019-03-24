c685df8f5be92bfdc185ad33d540bb9bbbe379cd
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-24 18:05:19 +0800

    bug fix

--- train_pre_global.sh
+++ train_pre_global.sh
@@ -3,7 +3,7 @@ do
 python3 -m global_model.train \
         --batch_size=8 --experiment_name=corefmerge \
         --training_name=group_global/global_trans_2_3_pre_model_v$v \
-        --evaluation_minutes=1 --nepoch_no_imprv=6 \
+        --evaluation_minutes=10 --nepoch_no_imprv=6 \
         --span_emb="boundaries" \
         --dropout=0.5 \
         --entity_extension=extension_entities \
