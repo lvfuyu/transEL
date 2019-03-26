d6b271437bea2d80a5a767642437520049690092
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-26 10:58:37 +0800

    pre global only

--- train_gt_global.sh
+++ train_gt_global.sh
@@ -2,12 +2,13 @@ for v in 1
 do
 python3 -m global_model.train \
         --batch_size=16 --experiment_name=corefmerge \
-        --training_name=group_global/global_trans_1_4_model_v$v \
+        --training_name=group_global/global_trans_1_3_model_v$v \
         --evaluation_minutes=10 --nepoch_no_imprv=6 \
         --span_emb="boundaries" \
         --dropout=0.5 \
         --entity_extension=extension_entities \
         --no_pre_training \
+        --no_use_local \
         --train_datasets=aida_train \
         --ed_datasets=aida_dev_z_aida_test --ed_val_datasets=0 --ed_test_datasets=1
 done
