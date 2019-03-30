603fcffa85d6fa86bb6d75891f23e6557a6acaef
福豆 <fuyu.lfy@alibaba-inc.com>
2019-03-30 15:03:14 +0800

    update

--- train_gt_global.sh
+++ train_gt_global.sh
@@ -9,6 +9,6 @@ python3 -m global_model.train \
         --entity_extension=extension_entities \
         --no_pre_training \
         --no_use_local \
-        --train_datasets=aida_train \
+        --train_datasets=aida_train_gt_after \
         --ed_datasets=aida_dev_z_aida_test --ed_val_datasets=0 --ed_test_datasets=0_1
 done
