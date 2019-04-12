b69cca4307a2e5b0de06e10459682d75680917d8
福豆 <fuyu.lfy@alibaba-inc.com>
2019-04-12 21:44:14 +0800

    update

--- train_end2end_global.sh
+++ train_end2end_global.sh
@@ -1,6 +1,6 @@
 python3 -m end2end_model.train \
     --batch_size=4   --experiment_name=corefmerge \
-    --training_name=group_global/global_trans_test_model_v1 \
+    --training_name=group_global/global_trans_end2end_model_v1 \
     --ent_vecs_regularization=l2dropout  --evaluation_minutes=1 --nepoch_no_imprv=6 \
     --span_emb="boundaries"  \
     --dim_char=50 --hidden_size_char=50 --hidden_size_lstm=150 \
