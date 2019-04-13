753de2f30575c360dceb5bc1cee3d167d9917eda
福豆 <fuyu.lfy@alibaba-inc.com>
2019-04-13 18:01:30 +0800

    fix

--- eval_new_end2end_global.sh
+++ eval_new_end2end_global.sh
@@ -2,6 +2,6 @@ python -m model.evaluate \
     --training_name=group_global/global_trans_new_end2end_model_v1 \
     --experiment_name=corefmerge \
     --entity_extension=extension_entities \
-    --ed_datasets=aida_dev_z_aida_test_z_aida_train_z_ace2004_z_aquaint_z_msnbc_z_wikipedia \
+    --ed_datasets=aida_dev_z_aida_test_z_aida_train_z_ace2004_z_aquaint_z_msnbc \
     --ed_val_datasets=0 --el_datasets="" --el_val_datasets=0
-#clueweb_z_
+#clueweb_z_ _z_wikipedia
